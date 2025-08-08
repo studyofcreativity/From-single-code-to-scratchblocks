f7a25323feadea6373eec8bf2baa3260fc18727c
studyofcreativity <study.of.creativity@gmail.com>
2025-08-07 22:13:06 -0400

    Update translator.js

---
+++ translator.js
@@ -0,0 +1,5 @@
+function translateToScratchblocks(decodedCode, language = "en") {
+  // En este ejemplo, simplemente pasamos el código ya convertido.
+  // Puedes expandir para hacer más compleja la traducción si lo deseas.
+  return decodedCode;
+}
